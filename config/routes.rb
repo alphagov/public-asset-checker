72fa198d78ccbe516c9015365e4f42737f017faf
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-01-17 11:52:43 +0000

    Configure healthcheck routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
+  get "/healthcheck/ready", to: GovukHealthcheck.rack_response
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
