f75ab60181cd3ee697c10485fd3d462069c3e52f
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-01-18 10:01:16 +0000

    Fix bug in validation

--- app/models/public_asset_status.rb
+++ app/models/public_asset_status.rb
@@ -1,7 +1,7 @@
 class PublicAssetStatus < ApplicationRecord
   belongs_to :public_asset
 
-  validates :public_asset_id, :validate_by, presence: true
+  validates :public_asset_id, presence: true
   validates :size, presence: true, unless: :version
   validates :version, presence: true, unless: :size
 end
