4611009d3d221dc5192bd8f400b37f40858e4800
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-01-18 16:27:49 +0000

    Add URL validation

--- app/models/public_asset.rb
+++ app/models/public_asset.rb
@@ -2,6 +2,7 @@ class PublicAsset < ApplicationRecord
   has_many :public_asset_statuses, dependent: :destroy
 
   validates :url, :validate_by, presence: true
+  validates :url, url: true
 
   def validate_by_size?
     validate_by == "size"
---
+++ app/models/url_validator.rb
@@ -0,0 +1,14 @@
+class UrlValidator < ActiveModel::EachValidator
+  def self.compliant?(value)
+    uri = URI.parse(value)
+    uri.is_a?(URI::HTTP) && !uri.host.nil?
+  rescue URI::InvalidURIError
+    false
+  end
+
+  def validate_each(record, attribute, value)
+    unless value.present? && self.class.compliant?(value)
+      record.errors.add(attribute, "is not a valid URL")
+    end
+  end
+end
