209b6f020283128a1d302c468452c037ce175c76
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-01-18 10:00:40 +0000

    Update seeds with more realistic timestamps

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,17 +18,19 @@ PublicAsset.create!(
 if Rails.env.development?
   require "faker"
 
-  to_date = Time.zone.today
-  from_date = to_date - 30
+  to_date_time = Time.zone.today
+  from_date_time = to_date_time - 30
 
   public_assets_by_size = PublicAsset.where(validate_by: "size")
   public_assets_by_version = PublicAsset.where(validate_by: "version")
 
-  (from_date..to_date).each do
+  (from_date_time..to_date_time).each do |date_time|
     public_assets_by_size.all.each do |asset|
       PublicAssetStatus.create!(
         public_asset_id: asset.id,
         size: Faker::Number.number(digits: 6),
+        created_at: date_time,
+        updated_at: date_time,
       )
     end
 
@@ -36,6 +38,8 @@ if Rails.env.development?
       PublicAssetStatus.create!(
         public_asset_id: asset.id,
         version: "v=#{Faker::Number.number(digits: 3)}",
+        created_at: date_time,
+        updated_at: date_time,
       )
     end
   end
