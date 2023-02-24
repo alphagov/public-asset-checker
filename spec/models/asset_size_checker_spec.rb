138757bf5bd503b744cfcba99b79186adedc852f
sonia hussain <sonia.hussain@digital.cabinet-office.gov.uk>
2023-02-24 09:58:10 +0000

    Added asset_version_compare test and refactored

--- spec/models/asset_size_checker_spec.rb
+++ spec/models/asset_size_checker_spec.rb
@@ -1,28 +1,6 @@
 require "spec_helper"
 require "faker"
 
-def stub_get(url, bytesize)
-  stub_request(:get, url).to_return(
-    status: 200,
-    body: Faker::Alphanumeric.alpha(number: bytesize),
-  )
-end
-
-def stub_post(category, new_value)
-  stub_request(:post, ENV["SLACK_WEBHOOK_URL"])
-    .with(body: payload(asset_status, category, new_value))
-    .to_return(status: 200, body: "")
-end
-
-def payload(asset_status, category, new_value)
-  { payload:
-    {
-      channel: ENV["SLACK_CHANNEL"],
-      username: ENV["SLACK_USERNAME"],
-      text: "#{category}: #{asset_status.public_asset.url} old [#{asset_status.size}] new [#{new_value}]",
-    }.to_json }
-end
-
 RSpec.describe AssetSizeChecker, type: :model do
   describe "initialize" do
     it "when created the checker has an empty notification array" do
@@ -36,19 +14,17 @@ RSpec.describe AssetSizeChecker, type: :model do
     let(:asset_status) { create(:public_asset_status) }
 
     it "when the response value is not the same and is within the tolerance we update the record and send a notification" do
-      bytesize = 0
-      stub_get(asset_status.public_asset.url, bytesize)
-      stub_post("UPDATE", bytesize)
-
+      stub_get(asset_status.public_asset.url, Faker::Alphanumeric.alpha(number: 0))
+      stub_post(asset_status.public_asset.url, "UPDATE", 0, asset_status.size)
+      
       checker.compare
 
       expect(checker.notifications.size).to eq(1)
     end
 
     it "when the response value is the same we send a notification" do
-      bytesize = 10
-      stub_get(asset_status.public_asset.url, bytesize)
-      stub_post("SAME", bytesize)
+      stub_get(asset_status.public_asset.url, Faker::Alphanumeric.alpha(number: 10))
+      stub_post(asset_status.public_asset.url, "SAME", 10, asset_status.size)
 
       checker.compare
 
@@ -56,9 +32,8 @@ RSpec.describe AssetSizeChecker, type: :model do
     end
 
     it "when the response value is not the same and not within the tolerance we send a warning notification" do
-      bytesize = 111
-      stub_get(asset_status.public_asset.url, bytesize)
-      stub_post("WARNING", bytesize)
+      stub_get(asset_status.public_asset.url, Faker::Alphanumeric.alpha(number: 111))
+      stub_post(asset_status.public_asset.url, "WARNING", 111, asset_status.size)
 
       checker.compare
 
