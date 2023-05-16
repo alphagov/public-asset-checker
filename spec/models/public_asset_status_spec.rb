78a18861e311182c4ec8d8fa23d42a1e252f7f50
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-05-16 08:27:04 +0100

    Add missing test for public asset value method

--- spec/models/public_asset_status_spec.rb
+++ spec/models/public_asset_status_spec.rb
@@ -2,4 +2,17 @@ RSpec.describe PublicAssetStatus, type: :model do
   it { is_expected.to validate_presence_of :public_asset_id }
   it { is_expected.to validate_presence_of :size }
   it { is_expected.to validate_presence_of :version }
+
+  describe "#value" do
+    let(:public_asset_status_size) { described_class.new(size: 123) }
+    let(:public_asset_status_version) { described_class.new(version: 'T="123"') }
+
+    it "returns the version" do
+      expect(public_asset_status_version.value).to eq 'T="123"'
+    end
+
+    it "returns the size" do
+      expect(public_asset_status_size.value).to eq 123
+    end
+  end
 end
