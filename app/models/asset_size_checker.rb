038a31cd351aa66b97a9aafbf91c7ec9bba3883f
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-05-17 16:51:02 +0100

    Simplify and improve Slack notification messages

--- app/models/asset_size_checker.rb
+++ app/models/asset_size_checker.rb
@@ -1,36 +1,56 @@
 require "faraday"
 
-class AssetSizeChecker < AssetChecker
-  def initialize
-    super(PublicAsset.sizes)
+class AssetSizeChecker
+  attr_reader :asset
+
+  def initialize(asset)
+    @asset = asset
   end
 
   def compare
-    public_assets.all.find_each do |asset|
-      response = Faraday.get(asset.url)
-      current = response.body.bytesize
-      expected = asset.latest_size
+    response = Faraday.get(asset.url)
+    current = response.body.bytesize
+    expected = asset.latest_size
 
-      category = "WARNING"
-      if current == expected
-        category = "SAME"
-      elsif within_tolerance?(current, expected)
-        create_public_asset_status(asset.id, current)
-        category = "UPDATE"
-      end
-      add_notification(category, current, expected, asset.url)
+    notification = Notification.new(asset.id, asset.url)
+    if expected == current
+      nothing_to_do(notification, current, expected)
+    elsif within_tolerance?(current, expected)
+      PublicAssetStatus.create!(public_asset_id: asset.id, size: current)
+      automatic_update(notification, current, expected)
+    else
+      action_required(notification, current, expected)
     end
-    notify
-  end
-
-private
-
-  def create_public_asset_status(public_asset_id, size)
-    PublicAssetStatus.create!(public_asset_id:, size:)
   end
 
   def within_tolerance?(current, expected)
     tolerance = ENV["SIZE_TOLERANCE"].to_i
     current.between?(expected - tolerance, expected + tolerance)
+  end
+
+private
+
+  def action_required(notification, current, expected)
+    notification.title = "Action required"
+    notification.color = "needs-attention"
+    notification.value = "Expected size is [#{expected}] Actual size is [#{current}]. Please check this script and update the <#{ENV['PRODUCTION_URL']}/public_assets/#{notification.id}|latest value> when you're happy that the situation is resolved."
+
+    notification
+  end
+
+  def automatic_update(notification, current, expected)
+    notification.title = "Automatic update"
+    notification.color = "other"
+    notification.value = "Expected size was [#{expected}] Actual size is [#{current}]. The expected value has been automatically updated to the new actual size."
+
+    notification
+  end
+
+  def nothing_to_do(notification, current, expected)
+    notification.title = "Nothing to do"
+    notification.color = "same"
+    notification.value = "Expected size is [#{expected}] Actual size is [#{current}]."
+
+    notification
   end
 end
--- lib/tasks/public_assets.rake
+++ lib/tasks/public_assets.rake
@@ -1,13 +1,21 @@
 namespace :public_assets do
   desc "Checks the current size of all known public assets."
   task check_all_sizes: :environment do
-    checker = AssetSizeChecker.new
-    checker.compare
+    notifier = Notifier.new
+    PublicAsset.sizes.all.find_each do |asset|
+      checker = AssetSizeChecker.new(asset)
+      notification = checker.compare
+      notifier.notify(notification)
+    end
   end
 
   desc "Checks the current version of all known public assets."
   task check_all_versions: :environment do
-    checker = AssetVersionChecker.new
-    checker.compare
+    notifier = Notifier.new
+    PublicAsset.versions.all.find_each do |asset|
+      checker = AssetVersionChecker.new(asset)
+      notification = checker.compare
+      notifier.notify(notification)
+    end
   end
 end
