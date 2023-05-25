2c04afcb7e0063cf5e54494cee3efd5b8eec604a
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-05-25 15:25:34 +0100

    Update controllers
    We can now add in the new regex fields, rename size, and simplify the
    values as presented to the graph.

--- app/controllers/public_assets_controller.rb
+++ app/controllers/public_assets_controller.rb
@@ -11,13 +11,7 @@ class PublicAssetsController < ApplicationController
 
     dates = statuses.map(&:created_at)
     @labels = dates.map { |date| Date.parse(date.to_s).strftime("%d/%m/%Y") }
-
-    if @public_asset.validate_by_size?
-      @values = statuses.map(&:size)
-    elsif @public_asset.validate_by_version?
-      versions = statuses.map(&:version)
-      @values = versions.map { |version| version.split("=").last.gsub('"', "").to_i }
-    end
+    @values = statuses.map(&:value)
   end
 
   def new
@@ -64,7 +58,7 @@ private
   end
 
   def public_asset_params
-    params.require(:public_asset).permit(:url, :validate_by)
+    params.require(:public_asset).permit(:url, :validate_by, :hosted_version_regex, :source_version_regex)
   end
 
   def run_rake(task)
